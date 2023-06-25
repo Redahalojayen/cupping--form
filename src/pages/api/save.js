c08657cd6859da35cb0e8d4d70676e404fb2c0a4
Redahalojayen <136948571+Redahalojayen@users.noreply.github.com>
2023-06-25 19:38:23 +0300

    fixed build7

--- src/pages/api/save.js
+++ src/pages/api/save.js
@@ -1,10 +1,13 @@
+import { join } from '@prisma/client/runtime';
+import { json } from 'express';
+
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
 console.log('enterd ');
 export default async function handler(req, res) {
-    let {   sampleName, roastLevel, fragranceAroma,  flavor, afterTaste,acidity,body,balance,uniformity,cleanCup,sweetness,overall,notes,} = req.body
+    let {   sampleName, roastLevel, fragranceAroma,  flavor, afterTaste,acidity,body,balance,uniformity,cleanCup,sweetness,overall,notes,} = JSON.parse( req.body)
   
   roastLevel = parseFloat(roastLevel);
   fragranceAroma =parseFloat(fragranceAroma);
